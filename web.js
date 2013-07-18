219488837e32ee451d700500d7d3df220ea34cf7
Ubuntu <ubuntu@ip-172-31-30-158.us-west-2.compute.internal>
2013-07-18 09:52:31 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var fs = require('fs');
-var buf = new Buffer(fs.readFileSync('index.html'));
+var buf = new Buffer(fs.readFileSync('bitstarter-hw4.html'));
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send(buf.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
