cfa1aedfc60b267caa7440fdc0be80406970e84a
Ubuntu <ubuntu@ip-172-31-30-158.us-west-2.compute.internal>
2013-07-19 09:26:07 +0000

    Read HW4 index.html file in web.js

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var fs = require('fs');
-var buf = new Buffer(fs.readFileSync('bitstarter-hw4.html'));
+var buf = new Buffer(fs.readFileSync('index.html'));
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
